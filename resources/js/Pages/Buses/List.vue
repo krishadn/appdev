0e7e6ff58d9d587a21a60a706072006848c09198
Rael <rafaeldronquillo@gmail.com>
2023-11-14 16:09:44 +0800

    Dashboard Available & scheduled bg; Bus management, Routes, Schedules, Locations, buttons hover effect

--- resources/js/Pages/Buses/List.vue
+++ resources/js/Pages/Buses/List.vue
@@ -30,7 +30,7 @@ function destroy(id){
                 </div>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                   <div class="p-3 ">
-                    <Link :href="route('bus.create')" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2 rounded mx-2" as="button">
+                    <Link :href="route('bus.create')" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2 rounded mx-2 transition duration-300 ease-in-out transform hover:scale-105" as="button">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
