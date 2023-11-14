c64e53851f243026c65d8337e2ef4c9c1666447f
Kesha Torriana <mktorriana@tip.edu.ph>
2023-11-14 23:00:31 +0800

    Added row status color coding, buttons info, and headers per page

--- resources/js/Pages/Schedules/List.vue
+++ resources/js/Pages/Schedules/List.vue
@@ -24,6 +24,9 @@ function destroy(id){
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="p-3 flex justify-center">
+                        <h1>BUS SCHEDULE</h1>
+                </div>
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                   <div class="p-3 ">
                     <Link :href="route('schedule.create')" class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2 rounded mx-2"
@@ -51,8 +54,16 @@ function destroy(id){
 
                             </tr>
                             </thead>
-                            <tbody class="bg-white divide-y" >
-                                <tr v-for="schedule in schedules" :key="schedule.id" class="border-b transition duration-300 ease-in-out hover:bg-neutral-300 dark:border-neutral-500 dark:hover:bg-neutral-600">
+                            <tbody class="bg-white divide-y">
+                                    <tr v-for="schedule in schedules" :key="schedule.id" 
+                                    :class="{
+                                        'border-b transition duration-300 ease-in-out hover:bg-neutral-300 dark:border-neutral-500 dark:hover:bg-neutral-600': true,
+                                        
+                                        'bg-green-300': schedule.status === 'In Transit',
+                                        'bg-blue-300': schedule.status === 'Arrived',
+                                        'bg-red-300': schedule.status === 'Delay',
+                                        'bg-yellow-300': schedule.status === 'Waiting',
+                                        }">   
                                     <td>
                                     {{ schedule.departure_time }}
                                     </td>
@@ -75,6 +86,7 @@ function destroy(id){
                                         <Link :href="route('schedule.edit')"
                                             class="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-semibold px-3 py-2  m-2 rounded mx-2"
                                             :data= "{id: schedule.id}" as="button" >
+                                            EDIT <!-- Edit info on button -->
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
                                            </svg>
@@ -82,7 +94,7 @@ function destroy(id){
 
                                             <PrimaryButton title="Delete Schedule" class="flex items-center bg-red-500 hover:bg-red-700 text-white font-semibold px-3 py-2 m-2 rounded mx-2"
                                                 @Click="destroy(schedule.id)"
-                                            >
+                                            > DELETE <!-- Delete info on button -->
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                             </svg>
